7f46468a11a65d7ee729a8c4f70c7df58026b434
leesangyeon1 <44923016+leesangyeon1@users.noreply.github.com>
2025-10-17 17:20:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,20 @@
 // Home((Hero, About, Experience, Projects, Skills, Contact)))
+import Hero from '../components/Hero';
+import SectionAbout from '../components/SectionAbout';
+import SectionContact from '../components/SectionContact';
+import SectionExperience from '../components/SectionExperience';
+import SectionProjects from '../components/SectionProjects';
+import SectionSkills from '../components/SectionSkills';
+
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+      <SectionAbout />
+      <SectionExperience />
+      <SectionProjects />
+      <SectionSkills />
+      <SectionContact />
+    </main>
+  );
+}
